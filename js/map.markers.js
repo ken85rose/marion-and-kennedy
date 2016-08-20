70d7ed2434c8913ae98cae7f6cdaafa97f011625
Kennedy Rose <kennedy@kennedyrose.com>
2016-08-20 16:42:17 -0500

    Initial commit

---
+++ js/map.markers.js
@@ -0,0 +1,152 @@
+//List of map markers
+var map_markers = [
+	/*
+	{
+		"title": "Reception",
+		"latitude":33.775664,
+		"longitude":-118.065090,
+		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Old Ranch Country Club <br> 29033 West Lake Drive, Agoura Hills, CA"
+	},
+	{
+		"title": "Accommodation 1",
+		"latitude":33.777929,
+		"longitude":-118.076891,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 1 <br> Rossmoor, CA 90720"
+	},
+	{
+		"title": "Accommodation 2",
+		"latitude":33.780428,
+		"longitude":-118.084075,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
+	},
+	{
+		"title": "Accommodation 3",
+		"latitude":33.777551,
+		"longitude":-118.050468,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
+	},
+	*/
+	{
+		"title": "Accommodation 1",
+		"latitude":38.034052,
+		"longitude":-87.538021,
+		"icon":"fa-bed",
+		"infoWindow":"Hampton Inn<br>5701 US-41, Evansville, IN 47711"
+	},
+	{
+		"title": "Accommodation 2",
+		"latitude":38.043669,
+		"longitude":-87.540619,
+		"icon":"fa-bed",
+		"infoWindow":"Holiday Inn<br>7101 US-41, Evansville, IN 47725"
+	},
+	{
+		"title": "Accommodation 3",
+		"latitude":38.012762,
+		"longitude":-87.53828,
+		"icon":"fa-bed",
+		"infoWindow":"Comfort Inn & Suites<br>3901 US-41, Evansville, IN 47711"
+	},
+	{
+		"title": "Accommodation 4",
+		"latitude":37.975454,
+		"longitude":-87.455707,
+		"icon":"fa-bed",
+		"infoWindow":"Hampton Inn<br>8000 Eagle Crest Blvd, Evansville, IN 47715"
+	},
+	{
+		"title": "Accommodation 5",
+		"latitude":37.974689,
+		"longitude":-87.4562367,
+		"icon":"fa-bed",
+		"infoWindow":"Hilton Garden Inn<br>220 Eagle Crest Dr, Evansville, IN 47715"
+	},
+	{
+		"title": "Accommodation 6",
+		"latitude":37.975224,
+		"longitude":-87.457288,
+		"icon":"fa-bed",
+		"infoWindow":"Fairfield Inn<br>7879 Eagle Crest Blvd, Evansville, IN 47715"
+	},
+	{
+		"title": "Accommodation 7",
+		"latitude":37.972755,
+		"longitude":-87.454666,
+		"icon":"fa-bed",
+		"infoWindow":"Courtyard Evansville East<br>8105 E Walnut St, Evansville, IN 47715"
+	},
+	{
+		"title": "Attraction 1",
+		"latitude":37.9730728,
+		"longitude":-87.5801777,
+		"icon":"fa-eye",
+		"infoWindow":"Riverfront Walkway<br>Riverfront Part, Evansville, IN 47708"
+	},
+	{
+		"title": "Attraction 2",
+		"latitude":37.980573,
+		"longitude":-87.593957,
+		"icon":"fa-eye",
+		"infoWindow":"Nut Club Fall Festival<br>316 N Wabash Avenue of Flags, Evansville, IN 47712"
+	},
+	{
+		"title": "Attraction 3",
+		"latitude":38.0385968,
+		"longitude":-87.6019111,
+		"icon":"fa-eye",
+		"infoWindow":"Mayse Farm Market<br>6400 N. St. Joseph Ave, Evansville, IN 47720"
+	},
+	{
+		"title": "Attraction 4",
+		"latitude":37.9716166,
+		"longitude":-87.5692635,
+		"icon":"fa-eye",
+		"infoWindow":"Children's Museum of Evansville<br>22 SE 5th St, Evansville, IN 47708"
+	},
+	{
+		"title": "Attraction 5",
+		"latitude":37.965371,
+		"longitude":-87.573351,
+		"icon":"fa-eye",
+		"infoWindow":"Evansville Museum of Art and History<br>411 SE Riverside Dr, Evansville, IN 47713"
+	},
+	{
+		"title": "Attraction 6",
+		"latitude":37.9676824,
+		"longitude":-87.5722309,
+		"icon":"fa-eye",
+		"infoWindow":"Reitz Home Museum<br>224 SE 1st St, Evansville, IN 47713"
+	},
+	{
+		"title": "Attraction 7",
+		"latitude":37.9531693,
+		"longitude":-87.5770375,
+		"icon":"fa-eye",
+		"infoWindow":"USS LST-325<br>840 LST Dr, Evansville, IN 47713"
+	},
+	{
+		"title": "Attraction 8",
+		"latitude":37.99634,
+		"longitude":-87.606021,
+		"icon":"fa-eye",
+		"infoWindow":"Mesker Park Zoo<br>1545 Mesker Park Dr, Evansville, IN 47720"
+	},
+	{
+		"title": "Transportation",
+		"latitude":38.0469933,
+		"longitude":-87.5293781,
+		"icon":"fa-plane",
+		"infoWindow":"Evansville Regional Airport <br> 7801 Bussing Dr, Evansville, IN 47725"
+	},
+	{
+		"title": "Ceremony",
+		"latitude":38.10066,
+		"longitude":-87.57649,
+		"icon":"fa-bell-o",
+		"infoWindow":"The Bauerhaus <br> 13605 Darmstadt Rd, Evansville, IN 47725"
+	}
+];
